7072877b51b821e261f149de2710401e028bd89a
Arturo Vidal <arturo.vidal.external@toyota-fs.com>
2024-04-16 13:10:45 +0200

    Updated filters and sorts

--- config.ts
+++ config.ts
@@ -1,13 +1,21 @@
 import IConfig from './src/interfaces/IConfig';
 
 export default {
+  global: {
+    locale: "es_ES"
+  },
   services: {
     github: {
       configuration: {
         nickname: 'TheArturthur',
+        fetcher: {
+          repositories: {
+            type: 'public'
+          }
+        },
         sort: {
           repositories: [
-            { attr: 'created_at', sortByDesc: false },
+            { attr: 'updated_at', sortByDesc: true },
             { attr: 'forks_count', sortByDesc: true },
             { attr: 'stargazers_count', sortByDesc: true },
           ],
@@ -16,8 +24,27 @@ export default {
           repositories: [
             [
               { attr: 'owner.login', values: ['TheArturthur'] },
-              { attr: 'name', values: ['awesome-vue', 'merge-conflict', 'github-pages-with-jekyll', 'markdown-portfolio', 'TheArturthur'], revert: true },
-              { attr: 'fork', values: true },
+              { attr: 'name', values: [
+                'awesome-vue', 
+                'merge-conflict', 
+                'github-pages-with-jekyll', 
+                'markdown-portfolio', 
+                'TheArturthur',
+                'TheArturthur.github.io',
+                'linkedin-skill-assessment-quizzes',
+                'Portada-UPM-ETSIINF',
+                'AntiPilarica',
+                'TFSDB',
+                'xpipe-storage',
+                'cardb-searcher',
+                'private',
+                'mobilo-web-code',
+                'web-cv',
+                'Spotify',
+                'Git-Talk17',
+                'PDLC',
+                'PDL_old'
+              ], revert: true }
             ],
           ],
         },
@@ -25,11 +52,18 @@ export default {
     },
   },
   data: {
-    position: 'Software Developer & DevOps Engineer',
+    first_name: 'Arturo',
+    last_name: 'Vidal',
+    bio: 'Software Developer & Devops enthusiast.\n I manage a homelab cluster as sysAdmin, monitoring and issue-resolver for friends and family using it.\n I\'m currently learning Kubernetes and preparing both the PCAP (Certified Associate Python Programmer) and RHCSA (Red Hat Certified System Administrator) certifications.',
+    gender: 'male',
+    position: 'Functional Analyst',
+    company: 'MSX International TechServices S.L.',
+    location: 'Madrid (Spain)',
+    hireable: true,
     links: [
       { name: 'github', url: 'https://github.com/TheArturthur' },
       { name: 'linkedin', url: 'https://www.linkedin.com/in/arturo-vidal-pe%C3%B1a-025a2aa3/' },
-      { name: 'twitter', url: 'https://twitter.com/TheArturthur' },
+      { name: 'twitter', url: 'https://twitter.com/TheArturthur' }
     ],
   },
   templates: {
@@ -38,5 +72,5 @@ export default {
         background: () => require('./public/background-picture.png'),
       },
     },
-  },
+  }
 } as IConfig;
