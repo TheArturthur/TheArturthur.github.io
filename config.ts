b84be46a97cd0db82901a3f46015ad00ee6e2ccd
Arturo Vidal <arturo.vidal.external@toyota-fs.com>
2024-04-16 18:19:36 +0200

    Refix multiline

--- config.ts
+++ config.ts
@@ -10,7 +10,7 @@ export default {
         nickname: 'TheArturthur',
         fetcher: {
           repositories: {
-            type: 'public'
+            type: 'all'
           }
         },
         sort: {
